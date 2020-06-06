f50fc76e00b9cfba355aefd791dfafd72008b7fd
Marek Żochowski <marek@zochowski.net>
2020-06-06 12:36:04 +0200

    Increase number of steps

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-docker build -f Dockerfile -t bert_perf_final .
+docker build -f Dockerfile -t bert_perf_final_4520 .
 
 docker run --gpus device=0 -v /home/zpp/deephol-data:/home/data \
--v /home/zpp/models_after_fine_tuning/bert_perf_final:/home/model -it bert_perf_final /bin/bash
+-v /home/zpp/models_after_fine_tuning/bert_perf_final_4520:/home/model -it bert_perf_final_4520 /bin/bash
 
 # Inside docker run: python3 experiments.py --dataset_dir=./data/deepmath/deephol/proofs/human/ --model_dir=./model
